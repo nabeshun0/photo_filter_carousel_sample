c4a11c0c0bd6be7129f5dc00bb63631942682b3b
snt-nbzw <sntnbzw@gmail.com>
2021-03-10 01:32:03 +0900

    Add:  a selector ring and dark gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,150 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: const ExampleInstagramFilterSelectionPage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class ExampleInstagramFilterSelectionPage extends StatefulWidget {
+  const ExampleInstagramFilterSelectionPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<ExampleInstagramFilterSelectionPage> {
+  final _filters = [
+    Colors.white,
+    ...List.generate(Colors.primaries.length,
+        (index) => Colors.primaries[(index * 4) % Colors.primaries.length])
+  ];
+
+  final _filterColor = ValueNotifier<Color>(Colors.white);
+
+  void _onFilterChanged(Color value) {
+    _filterColor.value = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _buildPhotoWithFilter(),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: _buildFilterSelector(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoWithFilter() {
+    return ValueListenableBuilder(
+      valueListenable: _filterColor,
+      builder: (context, value, child) {
+        final color = value as Color;
+        return Image.network(
+          'https://flutter.dev/docs/cookbook/img-files/effects/instagram-buttons/millenial-dude.jpg',
+          color: color.withOpacity(0.5),
+          colorBlendMode: BlendMode.color,
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterSelector() {
+    return FilterSelector(filters: _filters, onFilterChanged: _onFilterChanged);
+  }
+}
+
+@immutable
+class FilterSelector extends StatefulWidget {
+  const FilterSelector({
+    Key? key,
+    required this.filters,
+    required this.onFilterChanged,
+    this.padding = const EdgeInsets.symmetric(vertical: 24),
+  }) : super(key: key);
+
+  final List<Color> filters;
+  final void Function(Color selectedColor) onFilterChanged;
+  final EdgeInsets padding;
+
+  @override
+  _FilterSelectorState createState() => _FilterSelectorState();
+}
+
+class _FilterSelectorState extends State<FilterSelector> {
+  static const _filtersPerScreen = 5;
+  static const _viewportFractionPerItem = 1.0 / _filtersPerScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final itemSize = constraints.maxWidth * _viewportFractionPerItem;
+
+        return Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            _buildShadowGradient(itemSize),
+            _buildSelectionRing(itemSize),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildShadowGradient(double itemSize) {
+    return SizedBox(
+      height: itemSize * 2 + widget.padding.vertical,
+      child: const DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.transparent,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: SizedBox.expand(),
+      ),
+    );
+  }
+
+  Widget _buildSelectionRing(double itemSize) {
+    return IgnorePointer(
+      child: Padding(
+        padding: widget.padding,
+        child: SizedBox(
+          width: itemSize,
+          height: itemSize,
+          child: const DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(width: 6, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
