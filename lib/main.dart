551e251915169b612d463eb362cbc88cb2bfd6c6
snt-nbzw <sntnbzw@gmail.com>
2021-03-10 11:27:22 +0900

    Add: implement the filter carousel

--- lib/main.dart
+++ lib/main.dart
@@ -92,6 +92,29 @@ class _FilterSelectorState extends State<FilterSelector> {
   static const _filtersPerScreen = 5;
   static const _viewportFractionPerItem = 1.0 / _filtersPerScreen;
 
+  late final PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PageController(
+      viewportFraction: _viewportFractionPerItem,
+    );
+    _controller.addListener(_onPageChanged);
+  }
+
+  void _onPageChanged() {
+    final page = (_controller.page ?? 0).round();
+    widget.onFilterChanged(widget.filters[page]);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -102,6 +125,7 @@ class _FilterSelectorState extends State<FilterSelector> {
           alignment: Alignment.bottomCenter,
           children: [
             _buildShadowGradient(itemSize),
+            _buildCarousel(itemSize),
             _buildSelectionRing(itemSize),
           ],
         );
@@ -125,6 +149,64 @@ class _FilterSelectorState extends State<FilterSelector> {
         ),
         child: SizedBox.expand(),
       ),
+    );
+  }
+
+  Color itemColor(int index) => widget.filters[index % widget.filters.length];
+
+  void _onFilterTapped(int index) {
+    _controller.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 450),
+      curve: Curves.ease,
+    );
+  }
+
+  Widget _buildCarousel(double itemSize) {
+    return Container(
+      height: itemSize,
+      margin: widget.padding,
+      child: PageView.builder(
+          controller: _controller,
+          itemCount: widget.filters.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) {
+                    if (!_controller.hasClients ||
+                        !_controller.position.hasContentDimensions) {
+                      return SizedBox();
+                    }
+
+                    final selectedIndex = _controller.page!.roundToDouble();
+
+                    final pageScrollAmount = _controller.page! - selectedIndex;
+
+                    final maxScrollDistance = _filtersPerScreen / 2;
+
+                    final pageDistanceFromSelected =
+                        (selectedIndex - index + pageScrollAmount).abs();
+
+                    final percentFromCenter =
+                        1.0 - pageDistanceFromSelected / maxScrollDistance;
+
+                    final itemScale = 0.5 + (percentFromCenter * 0.75);
+                    final opacity = 0.25 + (percentFromCenter * 0.75);
+
+                    return Transform.scale(
+                      scale: itemScale,
+                      child: Opacity(
+                        opacity: opacity,
+                        child: FilterItem(
+                          color: itemColor(index),
+                          onFilterSelected: () => _onFilterTapped,
+                        ),
+                      ),
+                    );
+                  }),
+            );
+          }),
     );
   }
 
