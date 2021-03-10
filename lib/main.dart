a2ef96e2b8e33d611813d5471bd43d030c8a1a1e
snt-nbzw <sntnbzw@gmail.com>
2021-03-10 10:36:42 +0900

    Add: a filter carousel item

--- lib/main.dart
+++ lib/main.dart
@@ -148,3 +148,36 @@ class _FilterSelectorState extends State<FilterSelector> {
     );
   }
 }
+
+@immutable
+class FilterItem extends StatelessWidget {
+  FilterItem({
+    Key? key,
+    required this.color,
+    this.onFilterSelected,
+  }) : super(key: key);
+
+  final Color color;
+  final VoidCallback? onFilterSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onFilterSelected,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ClipOval(
+            child: Image.network(
+              'https://flutter.dev/docs/cookbook/img-files'
+              '/effects/instagram-buttons/millenial-texture.jpg',
+              color: color.withOpacity(0.5),
+              colorBlendMode: BlendMode.hardLight,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
